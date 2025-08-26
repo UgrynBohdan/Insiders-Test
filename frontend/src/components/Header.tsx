0fcf41932b4cfc4be0cbb9aa3b1be82fbe01b729
UgrynBohdan <ugrynbogdan7@gmail.com>
2025-08-26 14:40:05 +0300

    refactor: Add Link on user name

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,16 +7,18 @@ export default function Header() {
 
     return (
       <header className="w-full flex justify-between items-center p-4 shadow-md bg-white">
-          <Link to='/' className="text-blue-600 hover:underline mb-4 block">
+          <Link to='/'>
               <h1 className="text-xl font-bold">📚 Book Exchange</h1>
           </Link>
           <div>
               {user ? (
                   <div className="flex items-center gap-3">
-                      <span className="font-medium">Hi, {user.name} 👋</span>
-                      <Button variant="outline" onClick={logout}>
-                          Logout
-                      </Button>
+                    <Link to="/me/books">
+                        <span className="font-medium">Hi, {user.name} 👋</span>
+                    </Link>
+                    <Button variant="outline" onClick={logout}>
+                        Logout
+                    </Button>
                   </div>
               ) : (
                   <span className="text-gray-600">Not logged in</span>
